f45f1fd38169bead8c87957a611b49763f878b99
Ben Limmer <ben@benlimmer.com>
2018-08-20 10:26:57 -0600

    Use integration test vs. unit

---
+++ tests/integration/components/shrug-guy-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | shrug-guy', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`{{shrug-guy}}`);
+
+    assert.equal(this.element.textContent.trim(), '¯\\_(ツ)_/¯');
+  });
+
+  test('it yields to block content', async function(assert) {
+    await render(hbs`
+      {{#shrug-guy}}
+        <div data-test-yielded-content>Hello!</div>
+      {{/shrug-guy}}
+    `);
+
+    assert.equal(this.element.querySelector('div[data-test-yielded-content]').textContent.trim(), 'Hello!');
+  });
+});
